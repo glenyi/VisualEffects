d15293c484e9a87c3172009ba5b99831d855a196
Glen <glenyi81@gmail.com>
2014-10-01 21:24:29 -0400

    Initial Commit

---
+++ Visuals/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Visuals
+//
+//  Created by Glen Yi on 2014-10-01.
+//  Copyright (c) 2014 On The Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
