0b0957138308127b00e899fec3b21d482b493836
Glen <glenyi81@gmail.com>
2014-10-02 13:31:18 -0400

    Initial commit

--- Visuals/ViewController.swift
+++ Visuals/ViewController.swift
@@ -10,16 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var extraLightBlurView: UIVisualEffectView!
+    @IBOutlet var lightBlurView: UIVisualEffectView!
+    @IBOutlet var darkBlurView: UIVisualEffectView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func extraLightSwitchChanged(sender: UISwitch) {
+        self.extraLightBlurView.hidden = !sender.on
+    }
+    
+    @IBAction func lightSwitchChanged(sender: UISwitch) {
+        self.lightBlurView.hidden = !sender.on
+    }
+    
+    @IBAction func darkSwitchChanged(sender: UISwitch) {
+        self.darkBlurView.hidden = !sender.on
+    }
+    
 }
 
